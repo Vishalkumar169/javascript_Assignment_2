c84d9a38631a9c79dbe13032c92fa659ab9e5cb3
Vishal Kumar (2006169) <90318739+Vishalkumar169@users.noreply.github.com>
2021-11-27 22:17:46 +0530

    Create object.js
    Create an object ‘Person’ in Java Script,  which will have  ‘FirstName’, ’LastName’ and ‘Office Address’ as properties within it. Create three Persons and print all those three properties.

---
+++ object.js
@@ -0,0 +1,29 @@
+var person_1 = {
+    firstname: "Vishal",
+    lastname: "Kumar",
+    officeaddress: "Bihar"
+};
+
+console.log(person_1.firstname);
+console.log(person_1.lastname);
+console.log(person_1.officeaddress);
+
+var person_2 = {
+    firstname: "Rahul",
+    lastname: "Kumar",
+    officeaddress: "Jharkhand"
+};
+
+console.log(person_2.firstname);
+console.log(person_2.lastname);
+console.log(person_2.officeaddress);
+
+var person_3 = {
+    firstname: "Aman",
+    lastname: "Kumar",
+    officeaddress: "Rajashtan"
+};
+
+console.log(person_3.firstname);
+console.log(person_3.lastname);
+console.log(person_3.officeaddress);
